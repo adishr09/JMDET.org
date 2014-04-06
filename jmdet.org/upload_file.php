a36672d5773a98697bb37b0fdee0512963a069c8
archangel <adi.shr09@gmail.com>
2014-04-06 22:29:17 +0530

    All JMDET files

---
+++ jmdet.org/upload_file.php
@@ -0,0 +1,35 @@
+<?php
+$allowedExts = array("txt", "pdf", "doc", "docx");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if (($_FILES["file"]["size"] < 100000000) && in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+   else
+    {
+   /* echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>"; */
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+       move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      // echo "Stored in: " . "upload/" . $_FILES["file"]["name"]; 
+       header('Location: link.html');
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file <br> The file must be of .doc, .docx or .pdf extension and not more than 10 MB";
+  }
+?> 
